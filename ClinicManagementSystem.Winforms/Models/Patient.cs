2fb1873d9de46faf016403e0c3aff642a100d67e
Thu <thudinh.31241026994@st.ueh.edu.vn>
2026-06-02 15:38:47 +0700

    done

---
+++ ClinicManagementSystem.Winforms/Models/Patient.cs
@@ -0,0 +1,22 @@
+﻿namespace Models
+{
+    public class Patient : Person
+    {
+        public string PatientCode { get; set; }  
+
+        public string InsuranceNumber { get; set; }
+
+        public string BloodType { get; set; }       
+
+        // Liên hệ khẩn cấp
+        public string EmergencyContactName { get; set; }
+
+        public string EmergencyContactPhone { get; set; }
+
+        public string EmergencyContactRelation { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ ClinicManagementSystem.Winforms/Models/Receptionist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Receptionist : Person
+    {
+        public string ReceptionistCode { get; set; }
+
+        public string DepartmentId { get; set; }
+
+        public DateTime HireDate { get; set; }
+
+        public bool IsWorking { get; set; }
+    }
+}
